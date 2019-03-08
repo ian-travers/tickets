48b266d1f3882537f05673296842c15a1944cc9f
Ian Travers <mr.itpe@gmail.com>
2019-03-08 18:29:24 +0300

    Add two getter to Concert and test them

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -12,11 +12,29 @@ class ConcertTest extends TestCase
     public function test_can_get_formatted_date()
     {
         // Create a concert with a known date
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2019-03-10 8:00PM'),
         ]);
 
         // Verify the date is formatted as expected
-        $this->assertEquals('March 10, 2019', $date = $concert->formatted_date);
+        $this->assertEquals('March 10, 2019', $concert->formatted_date);
+    }
+
+    public function test_can_get_formatted_start_time()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2019-03-10 17:00:00'),
+        ]);
+
+        $this->assertEquals('5:00pm', $concert->formatted_start_time);
+    }
+
+    public function test_can_get_ticket_price_in_dollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750,
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
     }
 }
