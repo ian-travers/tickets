777d6a19538df8f0e00cb4cfefd6ec5e3fda5573
Ian Travers <mr.itpe@gmail.com>
2019-03-24 10:06:44 +0300

    Config for PaymentGateway and DB seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,14 @@
 <?php
 
+use App\Concert;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory(Concert::class)->states('published')->create()->addTickets(10);
     }
 }
