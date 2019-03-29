a53c81e6fca9e84c86e23ef7ab44b29071265731
Ian Travers <mr.itpe@gmail.com>
2019-03-29 17:38:32 +0300

    Decoupling data from Presentation (and HTML time tag)

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -9,7 +9,7 @@
                 <div class="m-xs-b-6">
                     <div class="flex-baseline flex-spaced p-xs-y-4 border-b">
                         <h1 class="text-xl">Order Summary</h1>
-                        <a href="#" class="link-brand-soft">{{ $order->confirmation_number }}</a>
+                        <a href="{{ url("/order/{$order->confirmation_number}") }}" class="link-brand-soft">{{ $order->confirmation_number }}</a>
                     </div>
                     <div class="p-xs-y-4 border-b">
                         <p><strong>Order Total: ${{ number_format($order->amount / 100, 2) }}</strong></p>
@@ -20,13 +20,14 @@
                 <div class="m-xs-b-7">
                     <h2 class="text-lg wt-normal m-xs-b-4">Your Tickets</h2>
 
+                    @php /* @var \App\Ticket $ticket */ @endphp
                     @foreach($order->tickets as $ticket)
 
                     <div class="card m-xs-b-5">
                         <div class="card-section p-xs-y-3 flex-baseline flex-spaced text-light bg-gray">
                             <div>
-                                <h1 class="text-xl wt-normal">No Warnings</h1>
-                                <p class="text-light-muted">with Cruel Hand and Backtrack</p>
+                                <h1 class="text-xl wt-normal">{{ $ticket->concert->title }}</h1>
+                                <p class="text-light-muted">{{ $ticket->concert->subtitle }}</p>
                             </div>
                             <div class="text-right">
                                 <strong>General Admission</strong>
@@ -42,11 +43,11 @@
                                         </div>
                                         <div class="media-body p-xs-l-4">
                                             <p class="wt-bold">
-                                                <time datetime="2019-03-31 19:00:00">
-                                                    Sunday, March 31th, 2019
+                                                <time datetime="{{ $ticket->concert->date->format('Y-m-d H:i') }}">
+                                                    {{ $ticket->concert->date->format('l, F jS, Y') }}
                                                 </time>
                                             </p>
-                                            <p class="text-dark-soft">Doors at 19:00pm</p>
+                                            <p class="text-dark-soft">Doors at {{ $ticket->concert->date->format('g:ia') }}}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -56,10 +57,10 @@
                                             @icon('location', 'text-brand-muted')
                                         </div>
                                         <div class="media-body p-xs-l-4">
-                                            <p class="wt-bold">Music Hall of Eurodance</p>
+                                            <p class="wt-bold">{{ $ticket->concert->venue }}</p>
                                             <div class="text-dark-soft">
-                                                <p>123 Marinenko st.</p>
-                                                <p>Polotsk, Belarus 211400</p>
+                                                <p>{{ $ticket->concert->venue_address }}</p>
+                                                <p>{{ $ticket->concert->city }}, {{ $ticket->concert->state }} {{ $ticket->concert->zip }}</p>
                                             </div>
                                         </div>
                                     </div>
@@ -68,7 +69,7 @@
                         </div>
                         <div class="card-section flex-baseline flex-spaced">
                             <p class="text-lg">{{ $ticket->code }}</p>
-                            <p>ivan.itpe@example.com</p>
+                            <p>{{ $order->email }}</p>
                         </div>
                     </div>
                     @endforeach
--- tests/Feature/ViewOrderTest.php
+++ tests/Feature/ViewOrderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Concert;
 use App\Order;
 use App\Ticket;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class ViewOrderTest extends TestCase
@@ -12,13 +13,24 @@ class ViewOrderTest extends TestCase
     function test_user_can_view_their_order_confirmation()
     {
         /** @var Concert $concert */
-        $concert = factory(Concert::class)->create();
+        $concert = factory(Concert::class)->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('March 12, 2017 8:00pm'),
+            'ticket_price' => 4250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+        ]);
 
         /** @var Order $order */
         $order = factory(Order::class)->create([
             'confirmation_number' => 'ORDERCONFIRMATION1234',
             'card_last_four' => '7072',
-            'amount' => 7500,
+            'amount' => 8500,
+            'email' => 'john@example.com',
         ]);
 
         /** @var Ticket $ticketA */
@@ -43,9 +55,19 @@ class ViewOrderTest extends TestCase
         });
 
         $response->assertSee('ORDERCONFIRMATION1234');
-        $response->assertSee('$75.00');
+        $response->assertSee('$85.00');
         $response->assertSee('**** **** **** 7072');
         $response->assertSee('TICKETCODE1234');
         $response->assertSee('TICKETCODE5678');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON');
+        $response->assertSee('17916');
+        $response->assertSee('john@example.com');
+
+        $response->assertSee('2017-03-12 20:00');
+        $response->assertSee('Doors at 8:00pm');
     }
 }
