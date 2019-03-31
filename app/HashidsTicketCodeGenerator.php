56dcec215b7afd7d48d341d5217c249dd3500cf8
Ian Travers <mr.itpe@gmail.com>
2019-03-31 18:56:27 +0300

    Ticket code generator test (with real generated code)

--- app/HashidsTicketCodeGenerator.php
+++ app/HashidsTicketCodeGenerator.php
@@ -3,11 +3,20 @@
 
 namespace App;
 
+use Hashids\Hashids;
 
 class HashidsTicketCodeGenerator implements TicketCodeGeneratorInterface
 {
+    private $hashids;
+
+    public function __construct($salt)
+    {
+        $this->hashids = new Hashids($salt, 6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    }
+
     public function generateFor($ticket)
     {
-        return 'AAAAAA';
+        /** @var Ticket $ticket */
+        return $this->hashids->encode($ticket->id);
     }
 }
--- tests/Unit/HashidsTicketCodeGeneratorTest.php
+++ tests/Unit/HashidsTicketCodeGeneratorTest.php
@@ -12,7 +12,7 @@ class HashidsTicketCodeGeneratorTest extends TestCase
     public function test_ticket_code_at_least_6_characters_long()
     {
         /** @var TicketCodeGeneratorInterface $ticketCodeGenerator */
-        $ticketCodeGenerator = new HashidsTicketCodeGenerator();
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
 
         $code = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
 
@@ -22,7 +22,7 @@ class HashidsTicketCodeGeneratorTest extends TestCase
     public function test_ticket_code_can_only_contain_uppercase_letters()
     {
         /** @var TicketCodeGeneratorInterface $ticketCodeGenerator */
-        $ticketCodeGenerator = new HashidsTicketCodeGenerator();
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
 
         $code = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
 
@@ -32,11 +32,36 @@ class HashidsTicketCodeGeneratorTest extends TestCase
     public function test_ticket_code_the_same_ticket_id_are_the_same()
     {
         /** @var TicketCodeGeneratorInterface $ticketCodeGenerator */
-        $ticketCodeGenerator = new HashidsTicketCodeGenerator();
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
 
         $code1 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
         $code2 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
 
         $this->assertEquals($code1, $code2);
+    }
+
+    public function test_ticket_code_for_different_ticket_id_are_different()
+    {
+        /** @var TicketCodeGeneratorInterface $ticketCodeGenerator */
+        $ticketCodeGenerator = new HashidsTicketCodeGenerator('testsalt1');
+
+        $code1 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 1]));
+        $code2 = $ticketCodeGenerator->generateFor(new Ticket(['id' => 2]));
+
+        $this->assertNotEquals($code1, $code2);
+    }
+
+    public function test_ticket_codes_generated_with_different_salts_are_different()
+    {
+        /** @var TicketCodeGeneratorInterface $ticketCodeGenerator1 */
+        $ticketCodeGenerator1 = new HashidsTicketCodeGenerator('testsalt1');
+
+        /** @var TicketCodeGeneratorInterface $ticketCodeGenerator2 */
+        $ticketCodeGenerator2 = new HashidsTicketCodeGenerator('testsalt2');
+
+        $code1 = $ticketCodeGenerator1->generateFor(new Ticket(['id' => 1]));
+        $code2 = $ticketCodeGenerator2->generateFor(new Ticket(['id' => 1]));
+
+        $this->assertNotEquals($code1, $code2);
     }
 }
