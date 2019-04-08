2096d8c2fb709541b7cf6d59969476db17f381d9
Ian Travers <mr.itpe@gmail.com>
2019-04-08 14:20:45 +0300

    Login Browser test (ExampleTest was deleted)

---
+++ tests/Browser/PromoterLoginTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class PromoterLoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    public function test_login_is_successfully()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'jane@example.com',
+            'password' => bcrypt('111111'),
+        ]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'jane@example.com')
+                ->type('password', '111111')
+                ->press('Log in')
+                ->assertPathIs('/backstage/concerts');
+        });
+    }
+
+    public function test_loggin_in_with_invalid_credentials()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'jane@example.com',
+            'password' => bcrypt('111111'),
+        ]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'jane@example.com')
+                ->type('password', 'wrong')
+                ->press('Log in')
+                ->assertPathIs('/login')
+                ->assertSee('credentials do not match');
+        });
+    }
+}
