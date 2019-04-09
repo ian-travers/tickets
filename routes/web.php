28e24b1fe745ea39b3d1e0e0f2c01daf5e864eca
Ian Travers <mr.itpe@gmail.com>
2019-04-09 11:43:38 +0300

    Added concert add form and view

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ Route::get('/concerts/{id}', 'ConcertController@show');
 Route::post('/concerts/{id}/orders', 'ConcertOrderController@store');
 Route::get('/orders/{confirmationNumber}', 'OrderController@show');
 
-Route::get('/login', 'Auth\LoginController@showLoginForm');
-Route::post('/login', 'Auth\LoginController@login');
-Route::post('/logout', 'Auth\LoginController@logout');
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('auth.show-login');
+Route::post('/login', 'Auth\LoginController@login')->name('auth.login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('auth.logout');
 
 Route::get('/backstage/concerts/new', 'Backstage\ConcertController@create');
