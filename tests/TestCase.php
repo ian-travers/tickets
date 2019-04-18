618466b15f686ef3c95e3acef9ef3604c961fbf5
Ian Travers <mr.itpe@gmail.com>
2019-04-18 15:01:31 +0300

    Modify TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Tests;
 
+use PHPUnit\Framework\Assert;
 use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -25,6 +27,22 @@ abstract class TestCase extends BaseTestCase
 
         TestResponse::macro('data', function ($key) {
             return $this->original->getData()[$key];
+        });
+
+        EloquentCollection::macro('assertContains', function ($value) {
+            Assert::assertTrue($this->contains($value), "Failed asserting that the collection contained the specified value");
+        });
+
+        EloquentCollection::macro('assertNotContains', function ($value) {
+            Assert::assertFalse($this->contains($value), "Failed asserting that the collection did not contain the specified value");
+        });
+
+        EloquentCollection::macro('assertEquals', function ($items) {
+            Assert::assertEquals(count($this), count($items));
+            $this->zip($items)->each(function ($pair) {
+                list($a, $b) = $pair;
+                Assert::assertTrue($a->is($b));
+            });
         });
     }
 }
